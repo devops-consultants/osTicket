984465de8401ca12880ff048458209ba24392070
Rob Coward <rob.coward@devops-consultants.co.uk>
2025-06-05 16:20:32 +0100

    More install.php template fixes

--- docker/files/data/bin/install.php
+++ docker/files/data/bin/install.php
@@ -188,7 +188,7 @@ if (!$configFile = file_get_contents($vars['config'])) {
   err("Failed to load configuration file: {$vars['config']}");
 };
 $configFile= str_replace("define('OSTINSTALLED',FALSE);","define('OSTINSTALLED',TRUE);",$configFile);
-$configFile= str_replace('%ADMIN-EMAIL%',$vars['admin_email'],$configFile);
+$configFile= str_replace('%ADMIN-EMAIL',$vars['admin_email'],$configFile);
 $configFile= str_replace('%CONFIG-DBHOST',$vars['dbhost'] . ':' . $vars['dbport'],$configFile);
 $configFile= str_replace('%CONFIG-DBNAME',$vars['dbname'],$configFile);
 $configFile= str_replace('%CONFIG-DBUSER',$vars['dbuser'],$configFile);
